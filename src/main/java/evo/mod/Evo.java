0307ac351ffcc0037e780da8785039c51c46e623
kenyonnystrom <55596156+kenyonnystrom@users.noreply.github.com>
2022-02-16 11:53:18 -0600

    Versioning update
    - Created new evo package with mod package inside it. Copied Evolution.java into mod package and changed its name and the package/class to evo, created mixins package next to it.
    
    - Created copy of evolution.mixins.json into evo.mixins.json, changed package to reflect name and removed all the mixins files (which were in client)
    
    - Updated fabric.mod.json authors and contact source; also changed “entrypoints”/”main” to evo.mod.Evo and “mixins” to evo.mixins.json; fixed “depends” to current versions of fabric and minecraft
    
    - Edited gradle.properties to use newest mc version, yarn mappings and loader version (and accompanying url to check these), as well as fabric API version
    
    - Edited build.gradle fabric-loom version to 0.11-snapshot, as suggested on the same url
    
    - Changed gradle-wrapper.properties (in gradle directory) to pull the 7.4 distribution from the url

---
+++ src/main/java/evo/mod/Evo.java
@@ -0,0 +1,13 @@
+package evo.mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Evo implements ModInitializer {
+
+    public static final String MOD_ID = "Evo";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Mod is initialized!");
+    }
+}
