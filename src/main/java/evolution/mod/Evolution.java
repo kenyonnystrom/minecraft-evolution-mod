c726a91e3f4f0c3a9aab48e3044314d51ffba4a5
bensantos <santosb@carleton.edu>
2022-02-20 11:07:53 -0600

    custom block that spreads on its own

--- src/main/java/evolution/mod/Evolution.java
+++ src/main/java/evolution/mod/Evolution.java
@@ -1,5 +1,6 @@
 package evolution.mod;
 
+import evolution.mod.blocks.ExampleBlock;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
@@ -13,12 +14,14 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Evolution implements ModInitializer {
-    public static final String MOD_ID = "evolution";//block set up
-    public static final Block EVOLUTION_SAPLING_BLOCK = new Block(FabricBlockSettings.of(Material.PLANT).strength(0F, 0f).sounds(BlockSoundGroup.GRASS).breakByTool(FabricToolTags.HOES).ticksRandomly());
+    public static final String MOD_ID = "evolution";
+    public static final ExampleBlock EXAMPLE_BLOCK = new ExampleBlock(FabricBlockSettings.of(Material.STONE).hardness(4.0f).ticksRandomly());
+
+    //public static final Block EVOLUTION_SAPLING_BLOCK = new Block(FabricBlockSettings.of(Material.PLANT).strength(0F, 0f).sounds(BlockSoundGroup.GRASS).breakByTool(FabricToolTags.HOES).ticksRandomly());
     @Override
     public void onInitialize() {
-        Registry.register(Registry.BLOCK, new Identifier(MOD_ID,"evolution_sapling_block"), EVOLUTION_SAPLING_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "evolution_sapling_block"), new BlockItem(EVOLUTION_SAPLING_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
+        Registry.register(Registry.BLOCK, new Identifier(MOD_ID,"evolution_sapling_block"), EXAMPLE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "evolution_sapling_block"), new BlockItem(EXAMPLE_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
         System.out.println("Mod is initialized!");
     }
 }
--- src/main/java/evolution/mod/blocks/ExampleBlock.java
+++ src/main/java/evolution/mod/blocks/ExampleBlock.java
@@ -2,26 +2,39 @@ package evolution.mod.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.LiteralText;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+
+
+import java.util.Random;
 
 public class ExampleBlock extends Block {
     public ExampleBlock(Settings settings) {
         //The actual parameters given to super() are used to initialize the inherited instance variables
         super(settings);
     }
-
     @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (!world.isClient) {
-            player.sendMessage(new LiteralText("Hello, world!"), false);
-        }
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random){
 
-        return ActionResult.SUCCESS;
+        //get position of current block
+        int block_x = pos.getX();
+        int block_y = pos.getY();
+        int block_z = pos.getZ();
+
+        //randomize offsets in nearby area
+        //no y-coordinate just yet
+        Random r = new Random();
+        int x_offset = r.nextInt(30) - 15;
+        int z_offset = r.nextInt(30) - 15;
+        //place a random block somewhere nearby
+        //spread custom block
+        BlockState new_block = this.getDefaultState();
+        BlockPos new_block_location = new BlockPos(block_x + x_offset, block_y,block_z + z_offset);
+        world.setBlockState(new_block_location, new_block);
+
     }
+
+
+
 }
+
