71b91d2606c3e3cdabafb49e3959ffe53bd86911
bensantos <santosb@carleton.edu>
2022-02-22 12:07:13 -0600

    renaming block

--- src/main/java/evolution/mod/blocks/EvolutionBlock.java
+++ src/main/java/evolution/mod/blocks/EvolutionBlock.java
@@ -8,8 +8,9 @@ import net.minecraft.util.math.BlockPos;
 
 import java.util.Random;
 
-public class ExampleBlock extends Block {
-    public ExampleBlock(Settings settings) {
+public class EvolutionBlock extends Block {
+
+    public EvolutionBlock(Settings settings, int gene1) {
         //The actual parameters given to super() are used to initialize the inherited instance variables
         super(settings);
     }
@@ -26,15 +27,12 @@ public class ExampleBlock extends Block {
         Random r = new Random();
         int x_offset = r.nextInt(30) - 15;
         int z_offset = r.nextInt(30) - 15;
+
         //place a random block somewhere nearby
         //spread custom block
         BlockState new_block = this.getDefaultState();
         BlockPos new_block_location = new BlockPos(block_x + x_offset, block_y,block_z + z_offset);
         world.setBlockState(new_block_location, new_block);
-
     }
-
-
-
 }
 
