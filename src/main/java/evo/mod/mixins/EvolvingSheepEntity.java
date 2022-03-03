7beaf973a9a21bfb263bee83a1e7fdbde739fc64
kenyonnystrom <55596156+kenyonnystrom@users.noreply.github.com>
2022-03-02 23:49:09 -0600

    Added Wool-Related methods to sheep and created WoolType to interact with the entity
    Using injections into many methods contained in SheepEntity for wool color purposes, similar methods of data storage were implemented for wool type. To work with these different wool types (and hopefully to connect them to different models for the sheep), a WoolType object was created based largely on the characteristics of DyeColor, which uses the same principles for coloration. Also, notably, sections (referred to as regions) were added to EvolvingSheepEntity to break up the code into different parts, as they all have to interact with one another and therefore must remain in one mixin file.

--- src/main/java/evo/mod/mixins/EvolvingSheepEntity.java
+++ src/main/java/evo/mod/mixins/EvolvingSheepEntity.java
@@ -2,85 +2,171 @@ package evo.mod.mixins;
 
 import evo.mod.DamageSourceExt;
 
+import evo.mod.WoolType;
+import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ExperienceOrbEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.passive.SheepEntity;
 
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.world.ServerWorld;
 
 import net.minecraft.util.math.BlockPos;
 
 import net.minecraft.world.GameRules;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.*;
+
+import java.util.Random;
 
 @Mixin(SheepEntity.class)
 public abstract class EvolvingSheepEntity
 extends AnimalEntity {
     private int realTick = 100;
     private int lifeTicks = 0;
+    private static final TrackedData<Byte> WOOL;
 
-    //We can decide on range in testing
-    //5 being avg, 0 being virtually no wool, 10 being super chunky
-    //start with 2: 0 = no wool, 1 = normal wool, 2 = heavy wool
-    private int woolDensity = 1;
-    //will need to decide how parents pass density on and how kid recieves
-
+    // Constructor
     public EvolvingSheepEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
     }
 
-    // Get temp of current location
+    //region WOOL-RELATED METHODS
+
+    // Register data tracking
+    static {
+        WOOL = DataTracker.registerData(EvolvingSheepEntity.class, TrackedDataHandlerRegistry.BYTE);
+    }
+
+    // Adding to sheepEntity data tracker initialization (which only tracks color), begin tracking Wool
+    @Inject(method="initDataTracker", at = @At("TAIL"))
+    public void initDataTracker(CallbackInfo ci) {
+        this.dataTracker.startTracking(WOOL, (byte)0);
+    }
+
+    // Sets wool type for this sheep (more info in WoolType.java) and adds it to tracker
+    public void setWool(WoolType wool) {
+        byte b = this.dataTracker.get(WOOL);
+        this.dataTracker.set(WOOL, (byte)(b & 240 | wool.getId() & 3));
+    }
+
+    // Gets wool type of this sheep from data tracker, returning WoolType
+    public WoolType getWool() {
+        return WoolType.byId(this.dataTracker.get(WOOL) & 3);
+    }
+
+    // If sheep is spawned in rather than birthed, randomly chooses its starting wool state
+    public WoolType generateDefaultWool(Random random) {
+        int i = random.nextInt(4);
+        return WoolType.byId(i);
+    }
+
+    // Save wool and age data to nbt so that it is saved when exiting Minecraft world
+    @Inject(method="writeCustomDataToNbt", at = @At("TAIL"))
+    public void writeCustomDataToNbt(NbtCompound nbt, CallbackInfo ci) {
+        nbt.putByte("Wool", (byte)this.getWool().getId());
+        nbt.putInt("Age", lifeTicks);
+    }
+
+    // Reinstate saved data upon reloading Minecraft world
+    @Inject(method="readCustomDataFromNbt", at = @At("TAIL"))
+    public void readCustomDataFromNbt(NbtCompound nbt, CallbackInfo ci) {
+        this.setWool(WoolType.byId(nbt.getByte("Wool")));
+        this.lifeTicks = nbt.getInt("Age");
+    }
+
+    // Add random generation of wool size to the spawning process
+    @Inject(method="initialize", at = @At("HEAD"))
+    public void initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, EntityData entityData, NbtCompound entityNbt, CallbackInfoReturnable<EntityData> cir) {
+        this.setWool(generateDefaultWool(world.getRandom()));
+    }
+    //endregion
+    //region TEMPERATURE-RELATED METHODS
+
+    // Get temp of this sheep's current location
     private float getTemp() {
         BlockPos pos = super.getBlockPos();
         Biome biome = world.getBiome(pos);
         return biome.getTemperature();
     }
 
-    // React to temperature.
+    // Get temperature, and take damage based on this sheep's wool type
     private void feelTemperature() {
         float currTemp = this.getTemp();
-        if (currTemp < 0F && woolDensity != 2) {
-            this.damage(DamageSourceExt.HYPOTHERMIA, 1.5F);
-        } else if (currTemp < 0.5F && woolDensity < 1) {
-            this.damage(DamageSourceExt.HYPOTHERMIA, 1.5F);
-        } else if (currTemp < 1F && woolDensity > 1) {
-            this.damage(DamageSourceExt.HEATSTROKE, 1.5F);
-        } else {
-            this.damage(DamageSourceExt.HEATSTROKE, 1.5F);
+        System.out.println(currTemp);
+        switch(this.getWool()) {
+            case NO_WOOL:
+                if (currTemp < 0.5F) {
+                    this.damage(DamageSourceExt.HYPOTHERMIA, 1.5F);
+                }
+                break;
+            case THIN_WOOL:
+                if (currTemp < 0.3F) {
+                    this.damage(DamageSourceExt.HYPOTHERMIA, 1.5F);
+                } else if (currTemp > 1.5F) {
+                    this.damage(DamageSourceExt.HEATSTROKE, 1.5F);
+                }
+                break;
+            case STD_WOOL:
+                if (currTemp < 0F) {
+                    this.damage(DamageSourceExt.HYPOTHERMIA, 1.5F);
+                } else if (currTemp > 0.8F) {
+                    this.damage(DamageSourceExt.HEATSTROKE, 1.5F);
+                }
+                break;
+            case THICK_WOOL:
+                if (currTemp > 0.5F) {
+                    this.damage(DamageSourceExt.HEATSTROKE, 1.5F);
+                }
+                break;
         }
     }
+    //endregion
+    //region LIFE-RELATED METHODS
 
     // Have a kid. Currently asexual
     private void giveBirth() {
-        // Asexually reproduce. Make this more randomized at some point
+        // Asexually reproduce
         PassiveEntity kid = this.createChild((ServerWorld) world, this);
         if (kid != null) {
             kid.setBaby(true);
+            // Boring Minecraft stuff to spawn in kid
             kid.refreshPositionAndAngles(this.getX(), this.getY(), this.getZ(), 0.0F, 0.0F);
             ((ServerWorld) world).spawnEntityAndPassengers(kid);
             world.sendEntityStatus(this, (byte) 18);
             if (world.getGameRules().getBoolean(GameRules.DO_MOB_LOOT)) {
                 world.spawnEntity(new ExperienceOrbEntity(world, this.getX(), this.getY(), this.getZ(), this.getRandom().nextInt(7) + 1));
             }
+
+            // Inherit parent wool (TO-DO:  IMPLEMENT RUDIMENTARY MUTATION)
+            EvolvingSheepEntity sheepKid = (EvolvingSheepEntity) kid;
+            sheepKid.setWool(this.getWool());
         }
     }
 
     // Called with tick; ages sheep and determines when they reproduce/die of old age
     private void increaseAge() {
-        lifeTicks--;
+        lifeTicks++;
         System.out.println(lifeTicks);
         // With re-framed ticks, an old-age val of 50 seems to work well. Remove this comment after testing is done
         if (lifeTicks > 10) {
-            // Die of old age
+            // Die of old age (TO-DO:  REMOVE ONCE SOME FORM OF PREDATION EXISTS)
             this.onDeath(DamageSourceExt.OUT_OF_WORLD);
             this.kill();
         } else if (lifeTicks == 6 || lifeTicks == 4) {
-            // 50-50 chance of reproducing
+            // 50-50 chance of reproducing (TO-DO:  CHANGE REPRODUCTION LIKELIHOOD)
             if (Math.random() > 0.5) {
                 this.giveBirth();
             }
@@ -88,7 +174,8 @@ extends AnimalEntity {
     }
 
     // Extension of mobTick in superclass, ticks every 0.2 seconds unadjusted
-    public void mobTick() {
+    @Inject(method="mobTick", at = @At("HEAD"))
+    public void mobTick(CallbackInfo ci) {
         // Reduce tick frequency by a factor specified in construction to reduce unnecessary load
         realTick--;
         if (realTick == 0) {
@@ -96,7 +183,8 @@ extends AnimalEntity {
 
             this.feelTemperature();
             this.increaseAge();
-            super.mobTick();
+            System.out.println(this.getWool().getName());
         }
     }
+    //endregion
 }
